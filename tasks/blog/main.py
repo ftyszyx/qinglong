f4813ab70588f19996ac19dbd249423e6bb91cc6
ftyszyx <whyzi@qq.com>
2024-07-13 12:38:03 +0800

    change proxy

--- tasks/blog/main.py
+++ tasks/blog/main.py
@@ -35,7 +35,8 @@ class BLog(TaskBase):
         # print(f'get res:{response.text}')
         json_data = response.json()
         download_url = json_data.get("assets")[0].get("browser_download_url")
-        download_url = "https://mirror.ghproxy.com/" + download_url
+        # download_url = "https://mirror.ghproxy.com/" + download_url
+        download_url = "https://gh.api.99988866.xyz" + download_url
         # download_url = "https://hub.gitmirror.com/" + download_url
         release_name = json_data.get("name")
         if dest_path is None or dest_path.strip() == "":
